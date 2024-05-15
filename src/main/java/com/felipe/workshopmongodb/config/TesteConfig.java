47ce82d6bf5d7088aa14eacfc45d3c58c7f03bc1
felipeam10 <felipeam10@hotmail.com>
2024-05-15 02:20:59 -0300

    vJSR-7.10 - Author, Comment and Post

--- src/main/java/com/felipe/workshopmongodb/config/TesteConfig.java
+++ src/main/java/com/felipe/workshopmongodb/config/TesteConfig.java
@@ -2,6 +2,7 @@ package com.felipe.workshopmongodb.config;
 
 
 import com.felipe.workshopmongodb.models.entities.User;
+import com.felipe.workshopmongodb.repositories.PostRepository;
 import com.felipe.workshopmongodb.repositories.UserRepository;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,9 +18,13 @@ public class TesteConfig {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PostRepository postRepository;
+
     @PostConstruct
     public void init() {
         userRepository.deleteAll();
+        postRepository.deleteAll();
 
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
