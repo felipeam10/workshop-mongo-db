026f5e146df0d50b28b3d846350f2a0561d3926a
felipeam10 <felipeam10@hotmail.com>
2024-05-16 01:40:17 -0300

    vJSR-7.11 - Database seeding

--- src/main/java/com/felipe/workshopmongodb/config/TesteConfig.java
+++ src/main/java/com/felipe/workshopmongodb/config/TesteConfig.java
@@ -1,6 +1,9 @@
 package com.felipe.workshopmongodb.config;
 
 
+import com.felipe.workshopmongodb.models.embedded.Author;
+import com.felipe.workshopmongodb.models.embedded.Comment;
+import com.felipe.workshopmongodb.models.entities.Post;
 import com.felipe.workshopmongodb.models.entities.User;
 import com.felipe.workshopmongodb.repositories.PostRepository;
 import com.felipe.workshopmongodb.repositories.UserRepository;
@@ -9,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 @Configuration
@@ -31,5 +35,20 @@ public class TesteConfig {
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post post1 = new Post(null, Instant.parse("2021-02-13T11:15:01Z"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new Author(maria));
+        Post post2 = new Post(null, Instant.parse("2021-02-14T10:05:49Z"), "Bom dia", "Acordei feliz hoje!", new Author(maria));
+
+        Comment c1 = new Comment("Boa viagem mano!", Instant.parse("2021-02-13T14:30:01Z"), new Author(alex));
+        Comment c2 = new Comment("Aproveite", Instant.parse("2021-02-13T15:38:05Z"), new Author(bob));
+        Comment c3 = new Comment("Tenha um ótimo dia!", Instant.parse("2021-02-14T12:34:26Z"), new Author(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2.getComments().addAll(Arrays.asList(c3));
+
+        postRepository.saveAll(Arrays.asList(post1, post2));
+
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepository.save(maria);
     }
 }
