d65281d8a6ed80526d8886d3c56acdc93d910f64
felipeam10 <felipeam10@hotmail.com>
2024-05-16 02:47:09 -0300

    vJSR-7.13 - GET user by id

--- src/main/java/com/felipe/workshopmongodb/services/UserService.java
+++ src/main/java/com/felipe/workshopmongodb/services/UserService.java
@@ -3,10 +3,12 @@ package com.felipe.workshopmongodb.services;
 import com.felipe.workshopmongodb.models.dto.UserDTO;
 import com.felipe.workshopmongodb.models.entities.User;
 import com.felipe.workshopmongodb.repositories.UserRepository;
+import com.felipe.workshopmongodb.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -18,6 +20,12 @@ public class UserService {
     public List<UserDTO> findAll() {
         List<User> list = repository.findAll();
         return list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+    }
+
+    public UserDTO findById(String id) {
+        Optional<User> result = repository.findById(id);
+        User entity = result.orElseThrow(() -> new ResourceNotFoundException("Objeto não encontrado"));
+        return new UserDTO(entity);
     }
 
 }
